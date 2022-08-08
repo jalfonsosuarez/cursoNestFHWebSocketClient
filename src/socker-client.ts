c7d7ff4fc32675fe73a20241d2fa9281b7534c7e
= <=>
2022-08-08 20:41:38 +0200

    WS front Teslo Shop

---
+++ src/socker-client.ts
@@ -0,0 +1,69 @@
+import { Manager, Socket } from "socket.io-client"
+
+let socket: Socket;
+
+export const connectToServer = (jwtToken: string) => {
+  const manager = new Manager('localhost:3000/socket.io/socket.io.js', {
+    extraHeaders: {
+      authentication: jwtToken
+    }
+  });
+
+  socket?.removeAllListeners();
+  socket = manager.socket('/');
+
+  addListeners();
+}
+
+const addListeners = () => {
+  const clientsUl = document.querySelector<HTMLUListElement>('#clients-ul')!;
+  const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
+  const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+  const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
+  const serverStatusLabel = document.querySelector('#server-status')!;
+
+  socket.on('connect', () => {
+    serverStatusLabel.innerHTML = 'connected';
+  });
+
+  socket.on('clients-updated', (clients: string[]) => {
+    let clientsHtml: string = '';
+    clients.forEach(clientId => {
+      clientsHtml += `
+        <li>${clientId}</li>
+      `;
+    });
+    clientsUl.innerHTML = clientsHtml;
+  });
+  
+  socket.on('disconnect', () => {
+    serverStatusLabel.innerHTML = 'disconnected';
+  });
+
+  messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (messageInput.value.trim().length <= 0) return;
+    socket.emit(
+      'message-from-client',
+      {
+        id: 'Yo mismo',
+        message: messageInput.value
+      }
+    );
+    messageInput.value = '';
+  });
+
+  socket.on('message-from-server',
+    (payload: { fullName: string, message: string }) => {
+      const newMessage = `
+      <li>
+        <strong>${payload.fullName}</strong>
+        <span>${payload.message}</span>
+      </li>
+      `;
+      const li = document.createElement('li');
+      li.innerHTML = newMessage;
+      messagesUl.append(li);
+    }
+  );
+}
